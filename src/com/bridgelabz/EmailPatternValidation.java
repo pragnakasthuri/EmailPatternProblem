667cf3065450f0c9112034d0213074958d0d0ade
pragnakasthruri <kpragna25@gmail.com>
2022-03-09 19:52:01 +0530

    Adding the code for UC5

--- src/com/bridgelabz/EmailPatternValidation.java
+++ src/com/bridgelabz/EmailPatternValidation.java
@@ -1,11 +1,13 @@
 package com.bridgelabz;
 /**
  * Validate Email Address with regex
- * Validate first mandatory part i.e; abc
- * Ensure @ and validate the mandatory second part i.e; bridgelabz
- * Ensure "." after bridgelabz and validate the mandatory third part i.e; co
- * Noe validate optional part i.e; xyz in abc.xyz@bridgelabz.co
+ * 1.Validate first mandatory part i.e; abc
+ * 2.Ensure @ and validate the mandatory second part i.e; bridgelabz
+ * 3.Ensure "." after bridgelabz and validate the mandatory third part i.e; co
+ * 4.Now validate optional part i.e; xyz in abc.xyz@bridgelabz.co
  * and make sure only following are valid special characters _,+,-,. preceding to xyz
+ * 5.Finally, validate the expression with supporting optional parts i.e;
+ * TLD only 2 characters
  */
 
 import java.util.ArrayList;
@@ -21,9 +23,9 @@ public class EmailPatternValidation {
      */
     public static boolean isValidEmail(String email) {
         /**
-         * Regex to check first mandatory part i.e; abc, @, bridgelabz, ".", co and xyz
+         * Regex to check first mandatory part i.e; abc, @, bridgelabz, ".", co and xyz and .in
          */
-        String emailRegex = "^[A-Za-z_+.-]+(?:[a-zA-Z]+)@[A-Za-z]+\\.[A-Za-z]{2,5}$";
+        String emailRegex = "^[A-Za-z]+(?:[-_+.a-zA-Z0-9]+)@([A-Za-z]+\\.)+[A-Za-z]{2,5}+(?:(\\.([A-Za-z]{2})))?$";
         /**
          * Compiling the ReGex
          */
@@ -60,9 +62,17 @@ public class EmailPatternValidation {
         /**
          * 2.Adding the email addresses to list which need to be validated
          */
-        emailAddressList.add("abc@bridgelabz.co");
+        emailAddressList.add("abc@bridgelabz.co.in");
+        emailAddressList.add("abc@yahoo.com");
+        emailAddressList.add("abc-100@yahoo.com");
+        emailAddressList.add("abc.100@yahoo.com");
+        emailAddressList.add("abc111@abc.com");
+        emailAddressList.add("abc-100@abc.net");
+        emailAddressList.add("abc.100@abc.com.au");
+        emailAddressList.add("abc.xyz@bridgelabz.co.in");
+
         emailAddressList.add("abc.xyz@bridgelabz.co");
-        emailAddressList.add("abc-xyz@bridgelabz.co");
+        emailAddressList.add("abc-xyz@bridgelabz.co.in");
         emailAddressList.add("abc_bridgelabz.company56");
         emailAddressList.add("ab33c@_bridgel4abz*gooogle");
 
