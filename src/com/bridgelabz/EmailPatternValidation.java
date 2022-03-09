477e7137071172cad13240feac0177eb9732cd30
pragnakasthruri <kpragna25@gmail.com>
2022-03-09 18:02:09 +0530

    Adding the code for validating optional part

--- src/com/bridgelabz/EmailPatternValidation.java
+++ src/com/bridgelabz/EmailPatternValidation.java
@@ -4,6 +4,8 @@ package com.bridgelabz;
  * Validate first mandatory part i.e; abc
  * Ensure @ and validate the mandatory second part i.e; bridgelabz
  * Ensure "." after bridgelabz and validate the mandatory third part i.e; co
+ * Noe validate optional part i.e; xyz in abc.xyz@bridgelabz.co
+ * and make sure only following are valid special characters _,+,-,. preceding to xyz
  */
 
 import java.util.ArrayList;
@@ -19,9 +21,9 @@ public class EmailPatternValidation {
      */
     public static boolean isValidEmail(String email) {
         /**
-         * Regex to check first mandatory part i.e; abc, @, bridgelabz, ".", and co
+         * Regex to check first mandatory part i.e; abc, @, bridgelabz, ".", co and xyz
          */
-        String emailRegex = "^[A-Za-z]*@[A-Za-z]*\\.[A-Za-z]{2,5}$";
+        String emailRegex = "^[A-Za-z_+.-]+(?:[a-zA-Z]+)@[A-Za-z]+\\.[A-Za-z]{2,5}$";
         /**
          * Compiling the ReGex
          */
@@ -59,6 +61,8 @@ public class EmailPatternValidation {
          * 2.Adding the email addresses to list which need to be validated
          */
         emailAddressList.add("abc@bridgelabz.co");
+        emailAddressList.add("abc.xyz@bridgelabz.co");
+        emailAddressList.add("abc-xyz@bridgelabz.co");
         emailAddressList.add("abc_bridgelabz.company56");
         emailAddressList.add("ab33c@_bridgel4abz*gooogle");
 
